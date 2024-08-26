ad637767b338a8e4c82c8959c2addac7e4f59733
Hazemsief <Hazemsief17@gmail.com>
2024-08-27 02:37:10 +0300

    Register Edited

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,10 +1,9 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { useFormik } from "formik";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import * as Yup from "yup";
 import { AuthContext } from '../../context/AuthContext';
-
 
 export default function Register() {
 
@@ -31,6 +30,34 @@ export default function Register() {
       .required("Confirmation password is required"),
   });
 
+  const navigate = useNavigate();
+  const { setaccessToken } = useContext(AuthContext);
+  const [apiError, setapiError] = useState("");
+  const [IsLoading, setIsLoading] = useState(false);
+
+  async function handelRegister(FormValues) {
+    setIsLoading(true);
+    try {
+      let { data } = await axios.post(`https://ecommerce.routemisr.com/api/v1/auth/signup`, FormValues);
+      console.log(data);
+      if (data.message === "success") {
+        setIsLoading(false);
+        setaccessToken(data.token);
+        navigate("/"); 
+      } else {
+        setIsLoading(false);
+        setapiError(data.message);
+      }
+    } catch (error) {
+      setIsLoading(false);
+      if (error.response) {
+        setapiError(error.response.data.message);
+      } else {
+        setapiError("Network Error");
+      }
+    }
+  }
+
   let formik = useFormik({
     initialValues: {
       name: "",
@@ -43,52 +70,25 @@ export default function Register() {
     onSubmit: handelRegister,
   });
 
-  const navigate = useNavigate();
-  const {setaccessToken} = useContext(AuthContext);
-  const [apiError, setapiError] = useState("");
-  const [IsLoading, setIsLoading] = useState(false);
-
-  async function handelRegister(FormValues) {
-    setIsLoading(true);
-    try {
-      let { data } = await axios.post(`https://ecommerce.routemisr.com/api/v1/auth/signin`, FormValues);
-      console.log(data);
-      if(data.message === "success"){
-        setIsLoading(false);
-        setaccessToken(data.token);
-        navigate("/"); 
-      }
-    } catch (apiResponse) {
-      setIsLoading(false);
-      setapiError(apiResponse?.response?.data?.message);
-      // console.log(apiResponse?.response?.data?.message);
-    }
-  }
-
-  const handleInputChange = (e) => {
-    formik.handleChange(e);
-    if (formik.touched[e.target.name]) {
-      formik.setFieldTouched(e.target.name, false, false);
-      formik.setFieldError(e.target.name, "");
-    }
-  };
-
   return (
     <>
       <form onSubmit={formik.handleSubmit} className="max-w-md mx-auto p-2">
         
-        {apiError ? <div className=" text-xl p-3 text-red-500 rounded-lg bg-gray-700">
-          {apiError}
-        </div> : null}
+        {apiError && (
+          <div className="text-xl p-3 text-red-500 rounded-lg bg-gray-700">
+            {apiError}
+          </div>
+        )}
 
         <h1 className="text-slate-950 text-3xl p-2 mb-2 font-semibold">
           Register Now
         </h1>
 
+        {/* Name Field */}
         <div className="relative z-0 w-full mb-5 group p-2">
           <input
             value={formik.values.name}
-            onChange={handleInputChange}
+            onChange={formik.handleChange}
             onBlur={formik.handleBlur}
             type="text"
             name="name"
@@ -104,14 +104,17 @@ export default function Register() {
             Enter Your Name
           </label>
         </div>
-        {formik.errors.name && formik.touched.name ? (
-          <div className="mt-2 mb-4 p-3 text-red-500 rounded-lg bg-gray-700">{formik.errors.name}</div>
-        ) : null}
+        {formik.errors.name && formik.touched.name && (
+          <div className="mt-2 mb-4 p-3 text-red-500 rounded-lg bg-gray-700">
+            {formik.errors.name}
+          </div>
+        )}
 
+        {/* Email Field */}
         <div className="relative z-0 w-full mb-5 group p-2">
           <input
             value={formik.values.email}
-            onChange={handleInputChange}
+            onChange={formik.handleChange}
             onBlur={formik.handleBlur}
             type="email"
             name="email"
@@ -127,14 +130,17 @@ export default function Register() {
             Enter Your Email
           </label>
         </div>
-        {formik.errors.email && formik.touched.email ? (
-          <div className="mt-2 mb-4 p-3 text-red-500 rounded-lg bg-gray-700">{formik.errors.email}</div>
-        ) : null}
+        {formik.errors.email && formik.touched.email && (
+          <div className="mt-2 mb-4 p-3 text-red-500 rounded-lg bg-gray-700">
+            {formik.errors.email}
+          </div>
+        )}
 
+        {/* Phone Field */}
         <div className="relative z-0 w-full mb-5 group p-2">
           <input
             value={formik.values.phone}
-            onChange={handleInputChange}
+            onChange={formik.handleChange}
             onBlur={formik.handleBlur}
             type="tel"
             name="phone"
@@ -150,14 +156,17 @@ export default function Register() {
             Enter Your Phone
           </label>
         </div>
-        {formik.errors.phone && formik.touched.phone ? (
-          <div className="mt-2 mb-4 p-3 text-red-500 rounded-lg bg-gray-700">{formik.errors.phone}</div>
-        ) : null}
+        {formik.errors.phone && formik.touched.phone && (
+          <div className="mt-2 mb-4 p-3 text-red-500 rounded-lg bg-gray-700">
+            {formik.errors.phone}
+          </div>
+        )}
 
+        {/* Password Field */}
         <div className="relative z-0 w-full mb-5 group p-2">
           <input
             value={formik.values.password}
-            onChange={handleInputChange}
+            onChange={formik.handleChange}
             onBlur={formik.handleBlur}
             type="password"
             name="password"
@@ -173,14 +182,17 @@ export default function Register() {
             Enter Your Password
           </label>
         </div>
-        {formik.errors.password && formik.touched.password ? (
-          <div className=" p-3 text-red-500 rounded-lg bg-gray-700">{formik.errors.password}</div>
-        ) : null}
+        {formik.errors.password && formik.touched.password && (
+          <div className="mt-2 mb-4 p-3 text-red-500 rounded-lg bg-gray-700">
+            {formik.errors.password}
+          </div>
+        )}
 
+        {/* RePassword Field */}
         <div className="relative z-0 w-full mb-5 group p-2">
           <input
             value={formik.values.rePassword}
-            onChange={handleInputChange}
+            onChange={formik.handleChange}
             onBlur={formik.handleBlur}
             type="password"
             name="rePassword"
@@ -193,19 +205,21 @@ export default function Register() {
             htmlFor="rePassword"
             className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-green-600 peer-focus:dark:text-green-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
-            Enter Your Confirmation Password
+            Re-Enter Your Password
           </label>
         </div>
-        {formik.errors.rePassword && formik.touched.rePassword ? (
-          <div className="mt-2 mb-4 p-3 text-red-500 rounded-lg bg-gray-700">{formik.errors.rePassword}</div>
-        ) : null}
+        {formik.errors.rePassword && formik.touched.rePassword && (
+          <div className="mt-2 mb-4 p-3 text-red-500 rounded-lg bg-gray-700">
+            {formik.errors.rePassword}
+          </div>
+        )}
 
+        {/* Submit Button */}
         <button
           type="submit"
-          className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
-          >
-          {IsLoading? <i className='fas fa-spinner fa-spin'></i> : 'submit'
-          }
+          className="w-full p-2 text-sm font-bold tracking-wide text-white bg-green-500 hover:bg-green-600 focus:bg-green-600 focus:outline-none focus:ring-0 focus:ring-green-300 focus:ring-opacity-50 dark:focus:ring-green-900"
+        >
+          {IsLoading ? <i className="fas fa-spinner fa-spin"></i> : "Register"}
         </button>
       </form>
     </>
